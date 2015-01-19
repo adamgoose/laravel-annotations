45f2fcb0ec36c825b9389725b32e83e74a9fb64c
Stef Horner <stef@tedslittlerobot.com>
2015-01-19 10:18:26 +0000

    tests for namespace scan parsing

--- tests/AnnotationServiceProviderTest.php
+++ tests/AnnotationServiceProviderTest.php
@@ -87,6 +87,55 @@ class AnnotationsServiceProviderTest extends PHPUnit_Framework_TestCase {
 
 		$this->assertEquals( ['classes'], $results );
 	}
+
+	public function testParseNamespaceScansBasic()
+	{
+		$this->provider = new AnnotationsServiceProviderAppNamespaceStub( $this->app );
+
+		$namespaces = [
+			'Foo', 'Bar'
+		];
+		$this->app->shouldReceive( 'make' )->with('path')->times(2)->andReturn( 'path/to/app' );
+		$this->app->shouldReceive( 'make' )
+			->with('Illuminate\Filesystem\ClassFinder')->times(2)
+			->andReturn( $classfinder = m::mock('Illuminate\Filesystem\ClassFinder') );
+
+		$classfinder->shouldReceive('findClasses')->with('path/to/app/Foo')->andReturn([ 'Foo\Foo', 'Foo\Bar' ]);
+		$classfinder->shouldReceive('findClasses')->with('path/to/app/Bar')->andReturn([ 'Bar\Foo', 'Bar\Bar' ]);
+
+
+		$results = $this->provider->parseNamespaceScans( $namespaces );
+
+		$this->assertEquals([
+			'Foo\Foo', 'Foo\Bar',
+			'Bar\Foo', 'Bar\Bar',
+		], $results);
+	}
+
+	public function testParseNamespaceScansFiltered()
+	{
+		$this->provider = new AnnotationsServiceProviderAppNamespaceStub( $this->app );
+
+		$namespaces = [
+			'Foo', 'Bar',
+			'except' => ['Foo\Bar', 'Baz']
+		];
+		$this->app->shouldReceive( 'make' )->with('path')->times(2)->andReturn( 'path/to/app' );
+		$this->app->shouldReceive( 'make' )
+			->with('Illuminate\Filesystem\ClassFinder')->times(2)
+			->andReturn( $classfinder = m::mock('Illuminate\Filesystem\ClassFinder') );
+
+		$classfinder->shouldReceive('findClasses')->with('path/to/app/Foo')->andReturn([ 'Foo\Foo', 'Foo\Bar' ]);
+		$classfinder->shouldReceive('findClasses')->with('path/to/app/Bar')->andReturn([ 'Bar\Foo', 'Bar\Bar' ]);
+
+
+		$results = $this->provider->parseNamespaceScans( $namespaces );
+
+		$this->assertEquals([
+			'Foo\Foo',
+			'Bar\Foo', 'Bar\Bar',
+		], $results);
+	}
 }
 
 
